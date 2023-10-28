0a65cbe3c36164e68e2cd883b0673dbbfad8d622
Thein Naing <theinnaing0908@gmail.com>
2023-10-28 11:31:18 +0800

    Update SQL Vendor Join.sql

--- SQL Vendor Join.sql
+++ SQL Vendor Join.sql
@@ -3,17 +3,21 @@ You should combine the values of the two tables based on the GroupID column.
 The final query should only print out the GroupID, CompanyName, and final count of all rows that are grouped into each company name under a column titled Count. 
 The output table should be then sorted by the Count column and then sorted by GroupID so that a higher number appears first.
 Your output should look like the following table. */
+/* write your SQL query below */
 
 SELECT 
-    maintable_UEX7Z.GroupID,
-    cb_vendorinformation.CompanyName,
-    COUNT(maintable_UEX7Z.GroupID) AS `Count`
-FROM 
-    maintable_UEX7Z
-JOIN cb_vendorinformation ON cb_vendorinformation.GroupID = maintable_UEX7Z.GroupID
-GROUP BY
-    maintable_UEX7Z.GroupID,
-    cb_vendorinformation.CompanyName
+ maintable_42VYZ.GroupID,
+cb_vendorinformation.CompanyName,
+COUNT( maintable_42VYZ.GroupID) AS `COUNT`
+
+FROM maintable_42VYZ
+JOIN cb_vendorinformation 
+  ON cb_vendorinformation.GroupID =  maintable_42VYZ.GroupID
+
+GROUP BY 
+ maintable_42VYZ.GroupID,
+ cb_vendorinformation.CompanyName
+
 ORDER BY 
-    COUNT(maintable_UEX7Z.GroupID),
-    maintable_UEX7Z.GroupID
+    COUNT(maintable_42VYZ.GroupID),
+    maintable_42VYZ.GroupID;
